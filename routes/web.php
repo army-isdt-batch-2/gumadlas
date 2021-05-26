dc1eec3bfb38f624c16cee7bd1db13c713b4b51c
Mjturno5793 <maryjanegumadlas@gmail.com>
2021-05-26 15:43:30 +0800

    Change routes

--- routes/web.php
+++ routes/web.php
@@ -16,74 +16,103 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login',[
     App\Http\Controllers\LoginController::class,
     'index'
-]);
+])->name('login');
 
 Route::get('/table',[
     App\Http\Controllers\LoginController::class,
     'table'
 
-]);
+])->name('table');
+
 Route::get('/employees',[
     App\Http\Controllers\LoginController::class,
     'employees'
-]);
+])->name('employees');
+
 Route::get('/department',[
     App\Http\Controllers\LoginController::class,
     'department'
-]);
+])->name('department');
+
 Route::get('/loans',[
     App\Http\Controllers\LoginController::class,
     'loans'
-]);
+])->name('loans');
+
 Route::get('/leaves',[
     App\Http\Controllers\LoginController::class,
     'leaves'
-]);
+])->name('leaves');
+
 Route::get('/deductions',[
     App\Http\Controllers\LoginController::class,
     'deductions'
-]);
+])->name('deductions');
+
 Route::get('/timekeeping',[
     App\Http\Controllers\LoginController::class,
     'timekeeping'
-]);
+])->name('timekeeping');
+
 Route::get('/payslip',[
     App\Http\Controllers\LoginController::class,
     'payslip'
-]);
+])->name('payslip');
+
+
+
 Route::get('/layout',[
     App\Http\Controllers\LoginController::class,
     'layout'
-]);
+])->name('layout');
+
+
+
 Route::get('/form',[
     App\Http\Controllers\LoginController::class,
     'form'
-]);
-Route::get('/department/create',[
+])->name('form');
+
+
+
+Route::post('/department/create',[
     App\Http\Controllers\LoginController::class,
-    'department_create'
-]);
-Route::get('/employees/create',[
+    'save'
+
+])->name('department.save');
+
+Route::post('/employees/create',[
     App\Http\Controllers\LoginController::class,
-    'employees_create'
-]);
-    Route::get('/loans/create',[
+    'save'
+
+])->name('employees.save');
+
+    Route::post('/loans/create',[
         App\Http\Controllers\LoginController::class,
-        'loans_create'
- ]);
- Route::get('/leaves/create',[
+        'save'
+
+ ])->name('loans.save');
+
+ Route::post('/leaves/create',[
      App\Http\Controllers\LoginController::class,
-     'leaves_create'
- ]);
-Route::get('/deductions/create',[
+     'save'
+
+ ])->name('leaves.save');
+
+Route::post('/deductions/create',[
     App\Http\Controllers\LoginController::class,
-    'deductions_create'
-]);
-Route::get('/timekeeping/create',[
+    'save'
+
+])->name('deductions.save');
+
+Route::post('/timekeeping/create',[
     App\Http\Controllers\LoginController::class,
-    'timekeeping_create'
-]);
-Route::get('/payslip/create',[
+    'save'
+
+])->name('timekeeping.save');
+
+Route::post('/payslip/create',[
     App\Http\Controllers\LoginController::class,
-    'payslip_create'
-]);
+    'save'
+
+])->name('payslip.save');
